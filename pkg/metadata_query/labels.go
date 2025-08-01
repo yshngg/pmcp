2773716b15c82011a184c5a13bd06a9aab8bc4a1
yshngg <yshngg@outlook.com>
2025-08-01 10:48:23 +0800

    💡(metadata_query): Add PR reference to TODO comment about LabelNames replacement [Explanation: Added GitHub PR link (#1850) to existing TODO comment to provide context for future type replacement from []string to model.LabelNames]

--- pkg/metadata_query/labels.go
+++ pkg/metadata_query/labels.go
@@ -28,7 +28,7 @@ type LabelNamesArguments struct {
 }
 
 type LabelNamesResult struct {
-	// TODO: replace []string with model.LabelNames ?
+	// TODO: replace []string with model.LabelNames ? https://github.com/prometheus/client_golang/pull/1850
 	LabelNames []string    `json:"names" jsonschema:"Names is a list of string label names."`
 	Warnings   v1.Warnings `json:"warnings,omitempty"`
 }
