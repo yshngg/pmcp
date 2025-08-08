823b0fe0c4e1b631b8dc07dff18b266ef4940930
yshngg <yshngg@outlook.com>
2025-08-08 10:09:08 +0800

    test: add testify dependency, fake prometheus api and time parsing tests

--- go.mod
+++ go.mod
@@ -8,13 +8,18 @@ require (
 	github.com/modelcontextprotocol/go-sdk v0.2.0
 	github.com/prometheus/client_golang v1.23.0
 	github.com/prometheus/common v0.65.0
+	github.com/stretchr/testify v1.10.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
 	github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pkg/utils/parse_time_test.go
@@ -0,0 +1,50 @@
+package utils
+
+import (
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseTime(t *testing.T) {
+	now := time.Now()
+	now = time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second(), 0, time.UTC)
+
+	testCases := []struct {
+		name     string
+		timeStr  string
+		expected time.Time
+		err      error
+	}{
+		{
+			"RFC3339",
+			"2006-01-02T15:04:05Z",
+			time.Date(2006, 1, 2, 15, 4, 5, 0, time.UTC),
+			nil,
+		},
+		{
+			"Unix Timestamp",
+			strconv.FormatInt(now.Unix(), 10),
+			now,
+			nil,
+		},
+		{
+			"Empty String",
+			"",
+			time.Date(1, 1, 1, 0, 0, 0, 0, time.UTC),
+			InvalidFormatError(""),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			actual, err := ParseTime(tc.timeStr)
+			require.Equal(t, tc.err, err)
+			require.Truef(t, tc.expected.Equal(actual), "expected: %s, actual: %s", tc.expected, actual)
+		})
+	}
+}
