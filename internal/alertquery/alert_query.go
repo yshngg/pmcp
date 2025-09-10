a12a3cb059d5a061db1eb29b49b2b475d5b952f9
Yusheng Guo <113574546+yshngg@users.noreply.github.com>
2025-09-10 22:26:48 +0000

    style: simply the implementations of handler

--- internal/alertquery/alert_query.go
+++ internal/alertquery/alert_query.go
@@ -13,12 +13,9 @@ type AlertQueryParams struct{}
 type AlertQueryResult = v1.AlertsResult
 
 func (d *alertQuerier) AlertQueryHandler(ctx context.Context, request *mcp.CallToolRequest, input *AlertQueryParams) (*mcp.CallToolResult, *AlertQueryResult, error) {
-	var (
-		result = &AlertQueryResult{}
-		err    error
-	)
-	if *result, err = d.API.Alerts(ctx); err != nil {
+	result, err := d.API.Alerts(ctx)
+	if err != nil {
 		return nil, nil, err
 	}
-	return nil, result, nil
+	return nil, &result, nil
 }
